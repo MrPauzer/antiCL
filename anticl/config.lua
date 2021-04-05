63f9ea5f1294335efaf6d62f52f57524a9a79561
Pauzer <63166329+MrPauzer@users.noreply.github.com>
2021-04-05 16:00:46 +0200

    Add files via upload

---
+++ anticl/config.lua
@@ -0,0 +1,14 @@
+------------------
+-----MrPauzer-----
+------------------
+Config = {}
+
+Config.DrawingTime = 20*1000 --20 sekund
+Config.TextColor = {r=255, g=255,b=255} -- Białt (Dane gracza)
+Config.AlertTextColor = {r=255, g=0, b=0} -- Czerwony(Gracz wyszedł z gry)
+Config.LogSystem = true
+Config.UseSteam = true -- Jak nie steam, to będzie pokazywało licencje
+Config.LogBotName = "pauzer_anticl"
+Config.AutoDisableDrawing = true
+Config.AutoDisableDrawingTime = 15000
+Config.webhook = ""
